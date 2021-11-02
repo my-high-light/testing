0dbcc4082550a31f81dadcdb64e44ccc3db254d6
my-high-light <85865051+my-high-light@users.noreply.github.com>
2021-11-02 14:15:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,48 @@ AOS.init({
 })
 //AOS
 
+/* Get this month and days of this month */
+const month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+
+const d = new Date();
+let thisMonth = month[d.getMonth()];
+
+let y = thisMonth;
+
+const DaysofNumber = new Array();
+DaysofNumber[0] = "31";
+DaysofNumber[1] = "28";
+DaysofNumber[2] = "31";
+DaysofNumber[3] = "30";
+DaysofNumber[4] = "31";
+DaysofNumber[5] = "31";
+DaysofNumber[6] = "30";
+DaysofNumber[7] = "31";
+DaysofNumber[8] = "30";
+DaysofNumber[9] = "31";
+DaysofNumber[10] = "30";
+DaysofNumber[11] = "31";
+
+let DaysofNumberThisMonth = DaysofNumber[d.getMonth()];
+
+let z = DaysofNumberThisMonth;
+
+/* Get this month and days of this month */
+
+document.getElementById("time").innerHTML = y + " " + z;
+
 /*
 //ROTATION LOGO
 const intro = document.querySelector('.intro');
